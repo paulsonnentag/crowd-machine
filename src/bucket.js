79a481dc8c1b0a016de747bec367bac19345ab08
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2013-10-09 10:37:13 +0200

    Adds bucket

---
+++ src/bucket.js
@@ -0,0 +1,69 @@
+(function () {
+  'use strict';
+
+  mmd.define('Bucket', [
+    'PhysicEntity'
+  ], function (PhysicEntity) {
+
+
+    var Bucket = PhysicEntity.extend({
+
+      init: function () {
+        this._super.apply(this, arguments);
+
+        // TODO: setup sensor
+
+
+
+      },
+
+      getSprite: function (params) {
+        var texture = new PIXI.Texture.fromImage('assets/img/funnel.png');
+        var sprite = new PIXI.Sprite(texture);
+
+        sprite.anchor = {
+          x: 0.5,
+          y: 0
+        };
+
+        sprite.position = {
+          x: params.x,
+          y: params.y
+        };
+
+        return sprite;
+      },
+
+      getPhysicDef: function (sprite) {
+
+        var leftEdge = PhysicEntity.getEdgeDef(
+          {x: -25, y: 0},
+          {x: -20, y: 50}
+        );
+
+        var rightEdge = PhysicEntity.getEdgeDef(
+          {x: 25, y: 0},
+          {x: 20, y: 50}
+        );
+
+        var bottomEdge = PhysicEntity.getEdgeDef(
+          {x: -20, y: 50},
+          {x: 20, y:  50}
+        );
+
+        return {
+          body: PhysicEntity.getBodyDef(sprite, PhysicEntity.TYPE.KINEMATIC),
+          fixtures: [
+            leftEdge,
+            rightEdge,
+            bottomEdge
+          ]
+        };
+      }
+    });
+
+    return Bucket;
+
+  });
+
+}());
